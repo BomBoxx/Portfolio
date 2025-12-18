6cc0b743efa74d27a01141dcf666ac898a4c08da
BomBoxx <abdelrahmanmarwan52@gmail.com>
2025-12-18 21:47:21 +0200

    Changing API endpoint

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("http://localhost:8000/auth/login", {
+  const res = await fetch("https://portfolio-admin-panel-1.onrender.com/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
